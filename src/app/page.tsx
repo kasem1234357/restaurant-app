d0351023c9c335c8198bb26cfdc580e7ee421721
kasem1234357 <kasemalolo2002@gmail.com>
2023-09-09 19:07:15 +0300

    update page.ts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       hello from here 
+       hello from here 2
     </main>
   )
 }
