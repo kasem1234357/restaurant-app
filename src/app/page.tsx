59fbb45be385dc0bfd4f184c0d465dac0535d8bb
kasem1234357 <kasemalolo2002@gmail.com>
2023-09-10 22:58:19 +0300

    config prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from 'next/image'
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-       hello from here 222 
+      hello from here 2223
     </main>
-  )
+  );
 }
