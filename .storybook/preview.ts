67ba00d5daadebe3b840852c31c6fa595792f652
kasem1234357 <kasemalolo2002@gmail.com>
2023-09-11 08:40:56 +0300

    storybook implementation and configuration

---
+++ .storybook/preview.ts
@@ -0,0 +1,49 @@
+import { MINIMAL_VIEWPORTS } from '@storybook/addon-viewport';
+import { Preview } from '@storybook/react';
+// import '../styles/_variables.scss'; // Import CSS variables
+
+// Custom viewports definition. Extends the MINIMAL_VIEWPORTS
+const customViewports = {
+  iphone7: {
+    name: 'iPhone 7',
+    styles: {
+      width: '375px',
+      height: '667px',
+    },
+  },
+  samsungGalaxyS21Ultra: {
+    name: 'Samsung Galaxy S21 Ultra',
+    styles: {
+      width: '384px',
+      height: '854px',
+    },
+  },
+};
+
+const preview: Preview = {
+  parameters: {
+    backgrounds: {
+      default: 'light',
+      values: [
+        { name: 'light', value: '#ffffff' },
+        { name: 'gray', value: '#808080' },
+        { name: 'dark', value: '#212b31' },
+      ],
+    },
+    actions: { argTypesRegex: '^on[A-Z].*' },
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/,
+      },
+    },
+    viewport: {
+      viewports: {
+        ...MINIMAL_VIEWPORTS,
+        ...customViewports,
+      },
+    },
+  },
+};
+
+export default preview;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import Image from "next/image";
-
+import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Link href={'/'}>home</Link>
       hello from here 2223
     </main>
   );
